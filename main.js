48157f145a60b0e23d3885d5ea7b5f8b72a3d674
hls93 <hlshaw3@ncsu.edu>
2017-07-24 15:47:40 -0400

    adding main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+const moment = require('./node_modules/moment');
+// console.log(moment);
+
+const chalk = require('./node_modules/chalk');
+// console.log(chalk.green('hello world'));
+
+console.log("it is " + chalk.yellow(moment().format("dddd, MMMM Do YYYY, h:mm:ss a")));
+console.log("it is the " + chalk.cyan(moment().format("DDD")) + " of the year");
+console.log("it is " + chalk.magentaBright(moment().format("X")) + " seconds into the day");
+
+if (moment(["YYYY"]).isLeapYear() === true){
+  console.log("it " + chalk.blue("is") + "a leap year");
+}
+else {
+  console.log("it " + chalk.red("is not ") + "a leap year");
+}
+
+if (moment(["LL"]).isDST() === true){
+  console.log("it " + chalk.green("is") + " day light savings time");
+}
+else {
+  console.log("it " + chalk.redBright("is not") + " day light savings time");
+}
